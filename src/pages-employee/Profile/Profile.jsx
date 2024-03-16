398257bf406c1069048a043dde3071b8ef0610b7
danielmihai11 <danielburtila@gmail.com>
2024-03-16 19:12:57 +0200

    My commit

--- src/pages-employee/Profile/Profile.jsx
+++ src/pages-employee/Profile/Profile.jsx
@@ -1,12 +1,154 @@
-import React from "react";
+import React, { useState } from "react";
 import ProfileCSS from "./Profile.module.css";
+import Account from "../../assets/account.svg";
+import Arrow from "../../assets/arrow.svg";
+import Email from "../../assets/email.svg";
 
 function Profile() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState("");
+  const [showSubOptions, setShowSubOptions] = useState(false);
+  const [selectedSubOption, setSelectedSubOption] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedModalOption, setSelectedModalOption] = useState("");
+
+  const toggleOptions = () => {
+    setIsOpen(!isOpen);
+    setShowSubOptions(false);
+  };
+
+  const handleOptionClick = (option) => {
+    setSelectedOption(option);
+    setShowSubOptions(true);
+  };
+
+  const handleSubOptionClick = (suboption) => {
+    setSelectedSubOption(suboption);
+    openModal();
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <div className={ProfileCSS.pageContainer}>
-      <div className={ProfileCSS.profileContainer}>
-        <p>Welcome to My Profile!</p>
+    <div className={ProfileCSS.profileContainer}>
+      <div className={ProfileCSS.container}>
+        <p className={ProfileCSS.message}>Hello, Burțilă Daniel!</p>
       </div>
+      <div className={ProfileCSS.card}>
+        <div className={ProfileCSS.iconContainer}>
+          <img src={Account} alt="Account" className={ProfileCSS.icon} />
+          <p className={ProfileCSS.employee}>Burțilă Daniel</p>
+        </div>
+
+        <div className={ProfileCSS.second_card}>
+          <div className={ProfileCSS.email_container}>
+            <img src={Email} alt="Email" className={ProfileCSS.email_icon} />
+            <p className={ProfileCSS.email_employee}>
+              daniel.burtila@student.usv.ro
+            </p>
+          </div>
+
+          <p className={ProfileCSS.function}>Employee</p>
+
+          <div className={ProfileCSS.skill_container}>
+            <p className={ProfileCSS.skill} onClick={toggleOptions}>
+              Skill
+              <img
+                src={Arrow}
+                alt="Arrow"
+                className={ProfileCSS.arrow_icon}
+                style={{ transform: isOpen ? "rotate(180deg)" : "none" }}
+              />
+            </p>
+            {isOpen && (
+              <ul className={ProfileCSS.skill_options}>
+                <li onClick={() => handleOptionClick("Programming Language")}>
+                  Programming Language
+                </li>
+                <li onClick={() => handleOptionClick("Libraries")}>
+                  Libraries
+                </li>
+                <li onClick={() => handleOptionClick("Frameworks")}>
+                  Frameworks
+                </li>
+                <li onClick={() => handleOptionClick("Software Engineering")}>
+                  Software Engineering
+                </li>
+              </ul>
+            )}
+            {showSubOptions && (
+              <div className={ProfileCSS.subOptions}>
+                <ul>
+                  <li onClick={() => handleSubOptionClick("C")}>C</li>
+                  <li onClick={() => handleSubOptionClick("C++")}>C++</li>
+                  <li onClick={() => handleSubOptionClick("Java")}>Java</li>
+                  <li onClick={() => handleSubOptionClick("Python")}>Python</li>
+                </ul>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+
+      {isModalOpen && (
+        <div className={ProfileCSS.modal}>
+          <div className={ProfileCSS.modal_content}>
+            <span className={ProfileCSS.close} onClick={closeModal}>
+              &times;
+            </span>
+            <h2 className={ProfileCSS.select_skill}>
+              Select the level for your skill:
+            </h2>
+
+            <div className={ProfileCSS.skill_level}>
+              <button onClick={() => handleSubOptionSelection("1 - Learns")}>
+                1 - Learns
+              </button>
+              <button onClick={() => handleSubOptionSelection("2 - Knows ")}>
+                2 - Knows
+              </button>
+              <button onClick={() => handleSubOptionSelection("3 - Does")}>
+                3 - Does
+              </button>
+              <button onClick={() => handleSubOptionSelection("4 - Helps")}>
+                4 - Helps
+              </button>
+              <button onClick={() => handleSubOptionSelection("5 - Teaches")}>
+                5 - Teaches
+              </button>
+            </div>
+            <h2>Select the level for your experience:</h2>
+            <div className={ProfileCSS.skill_experience}>
+              <button onClick={() => handleSubOptionSelection("0-6 months")}>
+                0-6 months
+              </button>
+              <button onClick={() => handleSubOptionSelection("6-12 months")}>
+                6-12 months
+              </button>
+              <button onClick={() => handleSubOptionSelection("1-2 years")}>
+                1-2 years
+              </button>
+              <button onClick={() => handleSubOptionSelection("2-4 years")}>
+                2-4 years
+              </button>
+              <button onClick={() => handleSubOptionSelection("4-7 years")}>
+                4-7 years
+              </button>
+              <button
+                onClick={() => handleSubOptionSelection(" more than 7 years")}
+              >
+                more than 7 years
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/pages-employee/SignInEmployee/SignInEmployee.jsx
+++ src/pages-employee/SignInEmployee/SignInEmployee.jsx
@@ -8,14 +8,36 @@ import { Link, useNavigate } from "react-router-dom";
 import Password from "../../assets/password.svg";
 ///import { useNavigate } from "react-router-dom";
 
-function SignInEmployee({ setSidebarActiveButton }) {
+function SignInEmployee() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
-  const [name, setName] = useState("");
 
   // const handleSignIn = () => {
   //   navigate("/dashboard");
   //   setSidebarActiveButton("/dashboard");
   // };
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await response.json();
+      if (data.error) {
+        alert(data.error);
+      }
+    } catch (error) {
+      alert("An error occurred");
+    }
+  }
   return (
     <div className={SignInEmployeeCSS.page}>
       <div className={SignInEmployeeCSS.container}>
@@ -31,14 +53,23 @@ function SignInEmployee({ setSidebarActiveButton }) {
 
           <h1 className={SignInEmployeeCSS.title}>Login to Your Account</h1>
 
-          <div className={SignInEmployeeCSS.signin_inputs}>
+          <form
+            className={SignInEmployeeCSS.signin_inputs}
+            onSubmit={handleSubmit}
+          >
             <div className={SignInEmployeeCSS.container_input}>
               <img
                 src={Email}
                 alt="Email"
                 className={SignInEmployeeCSS.input_logo}
-              ></img>
-              <input type="email" placeholder="Email" required />
+              />
+              <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              ></input>
             </div>
 
             <div className={SignInEmployeeCSS.container_input}>
@@ -46,16 +77,20 @@ function SignInEmployee({ setSidebarActiveButton }) {
                 src={Password}
                 alt="Password"
                 className={SignInEmployeeCSS.input_logo}
-              ></img>
-              <input type="password" placeholder="Password" required />
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              ></input>
             </div>
-          </div>
-          <Link
-            to="/dashboard-employee"
-            className={SignInEmployeeCSS.signin_btn_link}
-          >
-            <button className={SignInEmployeeCSS.signin_btn}>SIGN IN</button>
-          </Link>
+
+            <div className={SignInEmployeeCSS.signin_btn_link}>
+              <button className={SignInEmployeeCSS.signin_btn}>SIGN IN</button>
+            </div>
+          </form>
           <p className={SignInEmployeeCSS.signin_text}>
             You don't have an account?
             <Link
