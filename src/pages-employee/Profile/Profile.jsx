8a3e0c95bdf626a48a6af9d99960ffcf45ce80e4
SoldanCristina <cristina.soldan@yahoo.com>
2024-03-16 19:57:42 +0200

    Fixed conflicts

--- src/pages-employee/Profile/Profile.jsx
+++ src/pages-employee/Profile/Profile.jsx
@@ -7,38 +7,131 @@ import Email from "../../assets/email.svg";
 function Profile() {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedOption, setSelectedOption] = useState("");
-  const [showSubOptions, setShowSubOptions] = useState(false);
+  const [showCommonWindow, setShowCommonWindow] = useState(false);
+  const [selectedLevel, setSelectedLevel] = useState(0);
   const [selectedSubOption, setSelectedSubOption] = useState("");
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedModalOption, setSelectedModalOption] = useState("");
 
   const toggleOptions = () => {
     setIsOpen(!isOpen);
-    setShowSubOptions(false);
   };
 
   const handleOptionClick = (option) => {
     setSelectedOption(option);
-    setShowSubOptions(true);
+    setShowCommonWindow(true);
   };
-
-  const handleSubOptionClick = (suboption) => {
-    setSelectedSubOption(suboption);
-    openModal();
+  const handleLevelSelection = (level) => {
+    setSelectedLevel(level);
   };
-
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
+  const handleSubOptionSelection = (subOption) => {
+    setSelectedOption(subOption);
   };
 
   return (
     <div className={ProfileCSS.profileContainer}>
       <div className={ProfileCSS.container}>
         <p className={ProfileCSS.message}>Hello, Burțilă Daniel!</p>
+      </div>
+      <div className={ProfileCSS.card}>
+        <div className={ProfileCSS.iconContainer}>
+          <img src={Account} alt="Account" className={ProfileCSS.icon}></img>
+          <p className={ProfileCSS.employee}>Burțilă Daniel</p>
+        </div>
+
+        <div className={ProfileCSS.second_card}>
+          <div className={ProfileCSS.email_container}>
+            <img
+              src={Email}
+              alt="Email"
+              className={ProfileCSS.email_icon}
+            ></img>
+            <p className={ProfileCSS.email_employee}>
+              daniel.burtila@student.usv.ro
+            </p>
+          </div>
+
+          <p className={ProfileCSS.function}>Employee</p>
+
+          <div className={ProfileCSS.skill_container}>
+            <p className={ProfileCSS.skill} onClick={toggleOptions}>
+              Skill
+              <img
+                src={Arrow}
+                alt="Arrow"
+                className={ProfileCSS.arrow_icon}
+                style={{ transform: isOpen ? "rotate(180deg)" : "none" }}
+              ></img>
+            </p>
+            {isOpen && (
+              <ul className={ProfileCSS.skill_options}>
+                <li onClick={() => handleOptionClick("Programming Language")}>
+                  Programming Language
+                </li>
+                <li onClick={() => handleOptionClick("Libraries")}>
+                  Libraries
+                </li>
+                <li onClick={() => handleOptionClick("Frameworks")}>
+                  Frameworks
+                </li>
+                <li onClick={() => handleOptionClick("Software Engineering")}>
+                  Software Engineering
+                </li>
+              </ul>
+            )}
+
+            {showCommonWindow && (
+              <div className={ProfileCSS.common_window}>
+                <h2 className={ProfileCSS.select_level}>
+                  Select the level for your skill:
+                </h2>
+                <div className={ProfileCSS.levelButtons}>
+                  <button onClick={() => handleLevelSelection(1)}>
+                    1-Learns
+                  </button>
+                  <button onClick={() => handleLevelSelection(2)}>
+                    2-Knows
+                  </button>
+                  <button onClick={() => handleLevelSelection(3)}>
+                    3-Does
+                  </button>
+                  <button onClick={() => handleLevelSelection(4)}>
+                    4-Helps
+                  </button>
+                  <button onClick={() => handleLevelSelection(5)}>
+                    5-Teaches
+                  </button>
+                </div>
+
+                <h2 className={ProfileCSS.select_experience}>
+                  Select the level for your experience:
+                </h2>
+                <div className={ProfileCSS.subOptions}>
+                  <button
+                    onClick={() => handleSubOptionSelection("0-6 months")}
+                  >
+                    0-6 months
+                  </button>
+                  <button
+                    onClick={() => handleSubOptionSelection("6-12 months")}
+                  >
+                    6-12 months
+                  </button>
+                  <button onClick={() => handleSubOptionSelection("1-2 years")}>
+                    1-2 years
+                  </button>
+                  <button onClick={() => handleSubOptionSelection("2-4 years")}>
+                    2-4 years
+                  </button>
+                  <button onClick={() => handleSubOptionSelection("4-7 years")}>
+                    4-7 years
+                  </button>
+                  <button onClick={() => handleSubOptionSelection(">7 years")}>
+                    more than 7 years
+                  </button>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
       <div className={ProfileCSS.card}>
         <div className={ProfileCSS.iconContainer}>
