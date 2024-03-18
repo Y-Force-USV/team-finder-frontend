06e516a23916dca628be28f78940c9d5f9b0f8b1
SoldanCristina <cristina.soldan@yahoo.com>
2024-03-18 02:42:40 +0200

    Updated Departments component with modal functionality and  CSS style

--- src/pages-admin/Departments/Departments.jsx
+++ src/pages-admin/Departments/Departments.jsx
@@ -1,12 +1,13 @@
-// import React from "react";
 import React, { useState } from "react";
 import DepartmentsCSS from "./Departments.module.css";
+import Arrow from "../../assets/arrow.svg";
 import Search from "../../assets/search.svg";
 import Delete from "../../assets/delete.svg";
 import Modal from "../../components/Modal/Modal";
 
 function Departments() {
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [departments, setDepartments] = useState([]);
 
   const openModal = () => {
     setModalIsOpen(true);
@@ -16,31 +17,36 @@ function Departments() {
     setModalIsOpen(false);
   };
 
+  const addDepartment = (departmentName, departmentManager) => {
+    if (!departmentManager || !departmentName) return;
+    const newDepartment = {
+      name: departmentName,
+      manager: departmentManager,
+    };
+    setDepartments([...departments, newDepartment]);
+    closeModal();
+  };
+
   return (
     <div className={DepartmentsCSS.departments_container}>
-      {modalIsOpen && (
-        <div
-          className={`${DepartmentsCSS.departments_backdrop} ${DepartmentsCSS.active}`}
-        />
-      )}
+      {modalIsOpen && <div className={DepartmentsCSS.modal_background} />}
       <div className={DepartmentsCSS.message}>
-        <p> Here are your departments!</p>
+        <p>Here are your departments!</p>
       </div>
       <div className={DepartmentsCSS.right_buttons}>
         <div className={DepartmentsCSS.search_department}>
           <input
             type="text"
             name="searchTerm"
-            placeholder="  Search..."
+            placeholder="Search..."
             autoComplete="off"
             className={DepartmentsCSS.search_input}
           />
           <img
             className={DepartmentsCSS.search_img}
             src={Search}
-            width={"15px"}
-            height={"15px"}
-          ></img>
+            alt="Search"
+          />
         </div>
         <button
           className={`${DepartmentsCSS.add_btn} ${
@@ -48,19 +54,19 @@ function Departments() {
           }`}
           onClick={openModal}
         >
-          + ADD NEW{" "}
+          + ADD NEW
         </button>
         {modalIsOpen && (
           <div className={DepartmentsCSS.modal_container}>
-            <Modal isOpen={modalIsOpen} closeModal={closeModal} />
+            <Modal
+              isOpen={modalIsOpen}
+              closeModal={closeModal}
+              onSubmit={addDepartment}
+            />
           </div>
         )}
       </div>
-      <table
-        className={`${DepartmentsCSS.table} ${
-          modalIsOpen ? DepartmentsCSS.modalOpen : ""
-        }`}
-      >
+      <table className={DepartmentsCSS.table}>
         <thead>
           <tr className={DepartmentsCSS.header}>
             <th></th>
@@ -70,19 +76,24 @@ function Departments() {
           </tr>
         </thead>
         <tbody>
-          <tr className={DepartmentsCSS.departments}>
-            <td>Department</td>
-            <td>Frontend</td>
-            <td>Soldan Cristina</td>
-            <img
-              src={Delete}
-              alt="Delete"
-              className={DepartmentsCSS.delete}
-            ></img>
-          </tr>
+          {departments.map((department, index) => (
+            <tr key={index} className={DepartmentsCSS.departments}>
+              <td>Department</td>
+              <td>{department.name}</td>
+              <td>{department.manager}</td>
+              <td>
+                <img
+                  src={Delete}
+                  alt="Delete"
+                  className={DepartmentsCSS.delete}
+                />
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
   );
 }
+
 export default Departments;
